e58cfc324eb43ac7e9188a5f0fb1374c5052f53b
Jan Bielski <jan.bielski@edu.uekat.pl>
2022-05-06 23:04:08 +0200

    Adding Authorization and Authentication

--- BikeRentalServerAppUI/Program.cs
+++ BikeRentalServerAppUI/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using BikeRentalServerAppUI;
+using Microsoft.AspNetCore.Rewrite;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,6 +23,20 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseRewriter(
+	new RewriteOptions().Add(
+		context =>
+		{
+			if (context.HttpContext.Request.Path == "/MicrosoftIdentity/Account/SignedOut")
+			{
+				context.HttpContext.Response.Redirect("/");
+			}
+		}));
+
+app.MapControllers();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
--- BikeRentalServerAppUI/RegisterServices.cs
+++ BikeRentalServerAppUI/RegisterServices.cs
@@ -1,12 +1,28 @@
-﻿namespace BikeRentalServerAppUI;
+﻿using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+
+namespace BikeRentalServerAppUI;
 
 public static class RegisterServices
 {
 	public static void ConfigureServices(this WebApplicationBuilder builder)
 	{
 		builder.Services.AddRazorPages();
-		builder.Services.AddServerSideBlazor();
+		builder.Services.AddServerSideBlazor().AddMicrosoftIdentityConsentHandler();
 		builder.Services.AddMemoryCache();
+		builder.Services.AddControllersWithViews().AddMicrosoftIdentityUI();
+
+		builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+			.AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAdB2C"));
+
+		builder.Services.AddAuthorization(options =>
+		{
+			options.AddPolicy("Admin", policy =>
+			{
+				policy.RequireClaim("jobTitle", "Admin");
+			});
+		});
 
 		builder.Services.AddSingleton<IDbConnection, DbConnection>();
 		builder.Services.AddSingleton<ITypeData, MongoTypeData>();
